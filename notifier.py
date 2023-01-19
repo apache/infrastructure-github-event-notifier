d3562e55bd2e40607899e0e694fc7f9cc19c60bb
Daniel Gruno <humbedooh@apache.org>
2023-01-19 13:22:51 +0100

    Tweak From: header to include who caused this notification

--- notifier.py
+++ notifier.py
@@ -246,7 +246,7 @@ class Notifier:
             if SEND_EMAIL:
                 recipient = ml
                 asfpy.messaging.mail(
-                    sender="GitBox <git@apache.org>",
+                    sender=f"{user} (via GitHub) <git@apache.org>",
                     recipient=recipient,
                     subject=subject_line,
                     message=real_text,
